dc62fc954262f64b043c8d12faeced5e778e1782
Jonathan Ponader <Jpoander@knights.ucf.edu>
2019-10-21 14:32:30 -0400

    convolution generalization of function

--- testing.py
+++ testing.py
@@ -49,20 +49,20 @@ print(solved)
 
 # Convolution
 inputMat = np.array([[1,2,3,4],[-1,2,-3,1],[2,1,-2,-3],[4,-2,1,-4]])
-filter1 = np.array([[1,2],[2,1]])
-filter2 = np.array([[1,2],[1,1]])
-filter3 = np.array([[2,3],[1,1]])
-filter4 = np.array([[3,1],[1,3]])
+filters= np.array([	[[1,2],[2,1]],
+					[[1,2],[1,1]],
+					[[2,3],[1,1]],
+					[[3,1],[1,3]] ])
 
 inputlen = len(inputMat)
-filterLen = len(filter1)
-solution = Convolution(inputMat, filter1)
-solution2 = Convolution(inputMat, filter2)
+filterLen = len(filters[0])
+solution = Convolution(inputMat, filters[0])
+solution2 = Convolution(inputMat, filters[1])
 size = len(solution)
 
 print("---Convolution---")
 print(inputMat)
-print(filter1)
+print(filters[0])
 
 print("Solved Convolution Output")
 print(solution)
@@ -94,98 +94,46 @@ print(solved)
 # Solving Input 
 print("Solved Convolution Input")
 
-def SimpleConInputSolver():
+def SimpleConInputSolver(inputMat,filters):
 	#stride size only 1
-	solution = Convolution(inputMat, filter1)
-	solution2 = Convolution(inputMat, filter2)
-	solution3 = Convolution(inputMat, filter3)
-	solution4 = Convolution(inputMat, filter4)
 
-	solLen = len(solution)
-	filterLen = len(filter1)
+	filterLen = len(filters[0])
+	filterVar = filterLen*filterLen
+	weightMat = np.empty((filterVar,filterVar),dtype=np.int8)
+	answerMat = np.empty((filterVar),dtype=np.int8)
 
+	solution = []
+	count = 0
+
+	for filter in filters:
+		solution.append(Convolution(inputMat, filter))
+		flat = filter.flatten()
+		for z in range(0, filterVar):
+			weightMat[count][z] = flat[z]
+		count = count + 1
+
+	solution = np.array(solution)
+	solLen = len(solution[0])
 	# inputSize = ((output -1)*stride) - 2padding + filtersize
 	inputlen = ((solLen - 1)*1) + filterLen
-
 	solved = np.zeros((inputlen,inputlen))
-	weightMat = np.empty((filterLen*filterLen,filterLen*filterLen),dtype=np.int8)
-	answerMat = np.empty((filterLen*filterLen),dtype=np.int8)
-
-	for z in range(0,filterLen*filterLen):
-		weightMat[0][z] = filter1.flatten()[z]
-	for z in range(0,filterLen*filterLen):
-		weightMat[1][z] = filter2.flatten()[z]
-	for z in range(0,filterLen*filterLen):
-		weightMat[2][z] = filter3.flatten()[z]
-	for z in range(0,filterLen*filterLen):
-		weightMat[3][z] = filter4.flatten()[z]
 
 	for x in range(0,solLen):
 		for y in range(0,solLen):
-			answerMat[0] = solution[x][y]
-			answerMat[1] = solution2[x][y]
-			answerMat[2] = solution3[x][y]
-			answerMat[3] = solution4[x][y]
+			for z in range(0, filterVar):
+				answerMat[z] = solution[z][x][y]
 
 			intermed = np.linalg.solve(weightMat,answerMat)
 
-			solved[x][y] = intermed[0]
-			solved[x][y+1] = intermed[1]
-			solved[x+1][y] = intermed[2]
-			solved[x+1][y+1] = intermed[3]
+			count = 0
+			for i in range(0, filterLen):
+				for j in range(0, filterLen):
+					solved[x+i][y+j] = intermed[count]
+					count = count + 1
 
 	print(solved)
 
-
-def EffSimpleConInputSolver():
-	#does not work on matrixes that dont have perfect non overlapping fits
-	#stride size only 1
-
-	solution = Convolution(inputMat, filter1)
-	solution2 = Convolution(inputMat, filter2)
-	solution3 = Convolution(inputMat, filter3)
-	solution4 = Convolution(inputMat, filter4)
-
-	solLen = len(solution)
-	filterLen = len(filter1)
-
-	# inputSize = ((output -1)*stride) - 2padding + filtersize
-	inputlen = ((solLen - 1)*1) + filterLen
-
-	solved = np.zeros((inputlen,inputlen))
-	weightMat = np.empty((filterLen*filterLen,filterLen*filterLen),dtype=np.int8)
-	answerMat = np.empty((filterLen*filterLen),dtype=np.int8)
-
-	for z in range(0,filterLen*filterLen):
-		weightMat[0][z] = filter1.flatten()[z]
-	for z in range(0,filterLen*filterLen):
-		weightMat[1][z] = filter2.flatten()[z]
-	for z in range(0,filterLen*filterLen):
-		weightMat[2][z] = filter3.flatten()[z]
-	for z in range(0,filterLen*filterLen):
-		weightMat[3][z] = filter4.flatten()[z]
-
-	for x in range(0,solLen,filterLen):
-		for y in range(0,solLen, filterLen):
-			answerMat[0] = solution[x][y]
-			answerMat[1] = solution2[x][y]
-			answerMat[2] = solution3[x][y]
-			answerMat[3] = solution4[x][y]
-
-			intermed = np.linalg.solve(weightMat,answerMat)
-
-			solved[x][y] = intermed[0]
-			solved[x][y+1] = intermed[1]
-			solved[x+1][y] = intermed[2]
-			solved[x+1][y+1] = intermed[3]
-
-	print(solved)
-
-
-
-SimpleConInputSolver()
-EffSimpleConInputSolver()
-
+SimpleConInputSolver(inputMat, filters)
 
 
 
